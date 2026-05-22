4ba65bbc9ecf7083357a09b7fdfa2280d267aed5
DavidFox998 <davidjfox998@gmail.com>
2026-05-22 09:50:58 -0700

    Update lakefile.lean

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -13,4 +13,4 @@ lean_lib TheoremaAureum
 
 @[default_target]
 lean_exe «theorema-aureum-143» where
-  root := `Main
+  root := `TheoremaAureum.Main  ← This points to your current Main.lean location
