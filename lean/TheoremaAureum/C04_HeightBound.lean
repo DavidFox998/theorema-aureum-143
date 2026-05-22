5b17a8d0187fe478b8d8b36abdff2d7d5b509089
DavidFox998 <davidjfox998@gmail.com>
2026-05-22 11:09:06 -0700

    Update C04_HeightBound.lean

--- lean/TheoremaAureum/C04_HeightBound.lean
+++ lean/TheoremaAureum/C04_HeightBound.lean
@@ -4,8 +4,6 @@ import TheoremaAureum.RHStatement
 
 namespace TheoremaAureum
 
-open C03_Positivity
-
 /-- H2: Weil Positivity Transfer.
 "If VALOR > 0, then GRH holds for L(s,E)" -/
 axiom H2_WeilTransfer : 0 < VALOR → GRH_E_143a1
