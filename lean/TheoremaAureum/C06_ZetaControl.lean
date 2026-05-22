a9f1eee1f1601cfbaac019b4337b357e6ba68c6f
DavidFox998 <davidjfox998@gmail.com>
2026-05-22 08:59:55 -0700

    Update C06_ZetaControl.lean

--- lean/TheoremaAureum/C06_ZetaControl.lean
+++ lean/TheoremaAureum/C06_ZetaControl.lean
@@ -1,8 +1,7 @@
-  import C02_Modularity
-import C05_Discriminant
+import TheoremaAureum.C05_Discriminant
 
 namespace TheoremaAureum
 
 theorem C06_Zeta : C05_Disc (C04_Height (C03_Positive C01_BostBound (C02_Modular C01_BostBound))) → True := by sorry
 
-end TheoremaAureum  
+end TheoremaAureum
